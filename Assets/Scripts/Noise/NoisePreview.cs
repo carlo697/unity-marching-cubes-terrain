77e22705fcd0ab2d05a50e83977887334f637342
carlo697 <carlos.07888@gmail.com>
2023-05-07 14:28:47 -0400

    feat: add function to the NoisePreview component for testing the min and max values of the noise

--- Assets/Scripts/Noise/NoisePreview.cs
+++ Assets/Scripts/Noise/NoisePreview.cs
@@ -15,24 +15,31 @@ public class NoisePreview : MonoBehaviour {
 
   private MeshRenderer m_meshRenderer;
 
+  private BasicNoise m_basicNoise;
+  private FractalNoise m_fractalNoise;
+
   private void Start() {
     Generate();
   }
 
-  public void Generate() {
-    BasicNoise basicNoise = new BasicNoise(
+  private void InitializeNoises() {
+    m_basicNoise = new BasicNoise(
       frequency,
       amplitude,
       seed
     );
 
-    FractalNoise fractalNoise = new FractalNoise(
+    m_fractalNoise = new FractalNoise(
       frequency,
       amplitude,
       persistence,
       octaves,
       seed
     );
+  }
+
+  public void Generate() {
+    InitializeNoises();
 
     // Generate heightmap
     float[,] heightmap = new float[resolution, resolution];
@@ -44,7 +51,7 @@ public class NoisePreview : MonoBehaviour {
             (float)y * frequency + offset.y
           );
         } else if (type == NoiseType.Simple3D) {
-          float value = basicNoise.Sample(
+          float value = m_basicNoise.Sample(
             (float)x + offset.x,
             (float)y + offset.y,
             offset.z
@@ -52,7 +59,7 @@ public class NoisePreview : MonoBehaviour {
 
           heightmap[x, y] = (value + 1f) / 2f;
         } else {
-          float value = fractalNoise.Sample(
+          float value = m_fractalNoise.Sample(
             (float)x + offset.x,
             (float)y + offset.y,
             offset.z
@@ -77,5 +84,43 @@ public class NoisePreview : MonoBehaviour {
 
   private void OnValidate() {
     Generate();
+  }
+
+  [ContextMenu("Print Min And Max")]
+  private void PrintMinAndMax() {
+    InitializeNoises();
+
+    // Min and max values
+    float min = float.MaxValue;
+    float max = float.MinValue;
+    float median = 0;
+
+    // Generate heightmap
+    int samples = 50000000;
+    for (int i = 0; i < samples; i++) {
+      float finalValue = 0f;
+
+      float coordX = Random.Range(-10000000f, 10000000f);
+      float coordY = Random.Range(-10000000f, 10000000f);
+      float coordZ = Random.Range(-10000000f, 10000000f);
+
+      if (type == NoiseType.BuiltIn) {
+        finalValue = Mathf.PerlinNoise(coordX, coordY);
+      } else if (type == NoiseType.Simple3D) {
+        finalValue = m_basicNoise.Sample(coordX, coordY, coordZ);
+      } else {
+        finalValue = m_fractalNoise.Sample(coordX, coordY, coordZ);
+      }
+
+      median += finalValue / samples;
+      if (finalValue > max) {
+        max = finalValue;
+      } else if (finalValue < min) {
+        min = finalValue;
+      }
+    }
+
+    // Debug the min and max values
+    Debug.Log(string.Format("min: {0}, max: {1}, median: {2}", min, max, median));
   }
 }
