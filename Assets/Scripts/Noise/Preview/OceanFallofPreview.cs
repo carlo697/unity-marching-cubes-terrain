cb46595e5588e3d40c5ff949a3dbe4666360fc73
carlo697 <carlos.07888@gmail.com>
2023-06-03 22:52:50 -0400

    feat: add fields to the 'OceanFallofPreview' component to render the border of the ocean

--- Assets/Scripts/Noise/Preview/OceanFallofPreview.cs
+++ Assets/Scripts/Noise/Preview/OceanFallofPreview.cs
@@ -8,6 +8,11 @@ public class OceanFallofPreview : NoisePreview {
   public bool useOutputCurve;
   public AnimationCurve outputCurve = AnimationCurve.Linear(0f, 0f, 1f, 1f);
 
+  [Header("Ocean Border")]
+  public bool useSeaBorder;
+  public float seaBorderBeforeThreshold = 0.05f;
+  public float seaBorderAfterThreshold = 0.1f;
+
   public override void Generate() {
     System.Diagnostics.Stopwatch watch = new System.Diagnostics.Stopwatch();
     watch.Start();
@@ -18,28 +23,55 @@ public class OceanFallofPreview : NoisePreview {
     float[,] heightmap = new float[resolution, resolution];
     for (int y = 0; y < resolution; y++) {
       for (int x = 0; x < resolution; x++) {
-        float scale = frequency;
-
+        // Normalized coordinates used to sample the noise
         float normalizedX = ((float)x / resolution + falloffOffset.x) * falloffScale.x;
         float normalizedY = ((float)y / resolution + falloffOffset.y) * falloffScale.y;
 
+        // Clamped coordinates for creating the falloff map
         float posX = Mathf.Clamp01(normalizedX) * 2f - 1f;
         float posY = Mathf.Clamp01(normalizedY) * 2f - 1f;
 
+        // Create the falloff map
         float falloff = 1f - (1f - posX * posX) * (1f - posY * posY);
-        float curvedFalloff = curve.Evaluate(falloff);
+        float curvedFalloff = 1f - curve.Evaluate(falloff);
 
+        // Sample and normalize the noise
         float noise = (m_fastNoise2.GenSingle3D(
-          normalizedX * scale + offset.x,
-          normalizedY * scale + offset.y,
+          normalizedX * frequency + offset.x,
+          normalizedY * frequency + offset.y,
           0,
           seed
         ) + 1f) / 2f;
 
+        // Combine the falloff map and the noise
         // float finalFalloff = noise - curvedFalloff;
-        float finalFalloff = useFalloffOnly ? curvedFalloff : noise * (1f - curvedFalloff);
+        float finalFalloff = useFalloffOnly ? curvedFalloff : noise * curvedFalloff;
         if (useOutputCurve)
           finalFalloff = outputCurve.Evaluate(finalFalloff);
+
+        // Draw sea border
+        if (useSeaBorder) {
+          float borderGradient;
+          float start = threshold - seaBorderBeforeThreshold;
+          float middle = threshold;
+          if (finalFalloff <= start) {
+            borderGradient = 0f;
+          } else if (finalFalloff <= middle) {
+            borderGradient = Mathf.SmoothStep(
+              0f,
+              1f,
+              (finalFalloff - start) / (seaBorderBeforeThreshold)
+            );
+          } else {
+            borderGradient = Mathf.SmoothStep(
+              1f,
+              0f,
+              (finalFalloff - middle) / (seaBorderAfterThreshold)
+            );
+          }
+
+          finalFalloff = borderGradient;
+        }
 
         if (useThreshold) {
           heightmap[x, y] = finalFalloff >= threshold ? 1f : 0f;
